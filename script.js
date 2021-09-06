fd2144fc67f97d81db84a6008977f995ca98ecb7
Martin909 <>
2021-09-06 18:51:02 +0300

    creates a variable for month and day

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var month = 12
-
-var namesMale=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-
-var nameFemale=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama' ]
-
 var day = 31
 
+var name=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame','Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
 
+
+prompt("")
+
+function AkanName(name) {
+   return name;
+}
