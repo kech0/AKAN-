2f8c8f80551491db1657dbd0a6ec214b09d2d894
Martin909 <>
2021-09-06 18:25:52 +0300

    moves the script tags to the base of the code

---
+++ script.js
@@ -0,0 +1,10 @@
+var month = 12
+
+var namesMale=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+
+var nameFemale=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama' ]
+
+var day = 31
+
+
+
