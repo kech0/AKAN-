0bd89b7dffadab7ccbcb816de7c141eb2619f371
Martin909 <>
2021-09-08 08:12:32 +0300

    links to HTML to bootstrap

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-var month = 12
-var day = 31
-
-name=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame','Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
-
-
-
-prompt("")
-
-function AkanName(name) {
-   if (name) {
-
-   }
-}
-
+const frm = document.querySelector()
