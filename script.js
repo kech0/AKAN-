a775fa118cbad96810b1bb7cee113ae6656a25fb
Martin909 <>
2021-09-08 10:12:58 +0300

    adds functionality to the form

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const frm = document.querySelector()
+const frm = document.querySelector("#form1");
+const fd = new FormData(frm);
+
+
+frm.addEventListener("submit", function(e){
+    e.preventDefault();
+    console.log(fd.get("gender"));
+    
+})
